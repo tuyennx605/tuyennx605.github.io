f5cca30d0b3664ef54b5c87e5dbff45b07cb59d4
tuyennx605 <tuyennx605@gmail.com>
2018-06-28 12:47:48 +0700

    d

--- js30/20_recos/javascrip/scrip.js
+++ js30/20_recos/javascrip/scrip.js
@@ -3,10 +3,11 @@ const recognition = new SpeechRecogition();
 recognition.interimResults = true;
 
 recognition.addEventListener('result', e=>{
+	console.log(e.results);
 	const transcript = Array.from(e.results)
 			.map(result=>result[0])
 			.map(result=>result.transcript)
-			.join(' ');
+			.join('');
 
 	console.log(transcript);
 });
