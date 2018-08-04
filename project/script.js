7fc52aa2e059834f2a2faf5d23541898902c2d9d
tuyennx605 <32539309+tuyennx605@users.noreply.github.com>
2018-08-04 17:15:25 +0700

    peer

---
+++ project/script.js
@@ -0,0 +1,49 @@
+$(document).ready(()=>{
+    const openStream = ()=>{
+        const config = {audio: true, video:true};
+        return navigator.mediaDevices.getUserMedia(config);
+    }
+
+    const playStream = (idVideoTag, stream)=>{
+        const videotag = document.getElementById(idVideoTag);
+        videotag.srcObject = stream;
+        videotag.play();
+    }
+
+
+    // openStream()
+    // .then(stream=>playStream('localstream', stream));
+
+
+    var peer = new Peer();  //co id thi cho vao (id nay phai la khac nguoi khac nhe), ko thi no lay ngau nhien
+    peer.on('open', id=>$('#mypeer').append(id));
+
+//cau hinh nguoi goi:
+    $('#btncall').click(()=>{
+        const idThangKia = $('#remoteid').val();
+        console.log(idThangKia);
+        openStream()
+        .then(stream=>{
+            playStream('localstream', stream); // playstream minh 
+            var call = peer.call(idThangKia, stream);
+            call.on('stream', (remoteStream) =>{
+                playStream('remotestream', remoteStream); 
+            });
+
+        });
+    });
+
+
+
+//cau hinh khi co nguoi khac goi den
+peer.on('call', (call) =>{
+    openStream()
+    .then(stream=>{
+        call.answer(stream);
+        playStream('localstream', stream); // playstream minh 
+        call.on('stream', (remoteStream)=>  playStream('remotestream', remoteStream));
+    }, err=>console.log(err));
+
+  });
+
+});
